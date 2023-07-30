e12cb345e7999be4a771d12294fc088f667b2ba0
LeHien2818 <hienltv281823@gmail.com>
2023-07-30 20:07:34 +0700

    update slider

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import slide1 from '../assets/Slide1.png';
+import slide2 from '../assets/Slide2.png';
+import slide3 from '../assets/Slide3.png';
+import slide4 from '../assets/Slide4.png';
+import slide5 from '../assets/Slide5.png';
+import slide6 from '../assets/Slide6.png';
+import slide7 from '../assets/Slide7.png';
 import hrtLogo from '../assets/hrt_logo.png'
 import CustomCarousel from './CustomCarousel';
 
@@ -10,14 +16,34 @@ const items = [
     caption: 'Slide 1'
   },
   {
-    src: slide1,
+    src: slide2,
     altText: 'Slide 2',
     caption: 'Slide 2'
   },
   {
-    src: slide1,
+    src: slide3,
     altText: 'Slide 3',
     caption: 'Slide 3'
+  },
+  {
+    src: slide4,
+    altText: 'Slide 4',
+    caption: 'Slide 4'
+  },
+  {
+    src: slide5,
+    altText: 'Slide 5',
+    caption: 'Slide 5'
+  },
+  {
+    src: slide6,
+    altText: 'Slide 6',
+    caption: 'Slide 6'
+  },
+  {
+    src: slide7,
+    altText: 'Slide 7',
+    caption: 'Slide 7'
   }
 ];
 export const AboutMe = () => {
