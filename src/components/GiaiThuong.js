ce59cb21a8ad93bd49ce1d8f88bfe74c34070fe1
hieumai943 <hieunm.hrt@gmail.com>
2023-08-05 12:34:27 +0700

    done the le( responsive)

--- src/components/GiaiThuong.js
+++ src/components/GiaiThuong.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import achieve1 from "../assets/gold.png";
+import achieve2 from "../assets/silver.png";
+import achieve3 from "../assets/copper.png";
 
 export const GiaiThuong = () => {
   return (
@@ -7,15 +9,24 @@ export const GiaiThuong = () => {
         
         <div className="timeline">
             <h1>GIẢI THƯỞNG</h1>
+            <div class="achieve">
+            <img src={achieve1} />
             <p>
                 01 giải Nhất: 10,000,000 VND tiền mặt + giấy chứng nhận + kỷ niệm chương.
             </p>
+            </div>
+            <div class="achieve">
+            <img src={achieve2} />
             <p>
                 01 giải Nhì: 6,000,000 VND tiền mặt + giấy chứng nhận + kỷ niệm chương.
             </p>
+            </div>
+            <div class="achieve">
+            <img src={achieve3} />
             <p>
                  01 giải Ba: 4,000,000 VND tiền mặt + giấy chứng nhận + kỷ niệm chương.
             </p>
+            </div>
         </div>
         
     
