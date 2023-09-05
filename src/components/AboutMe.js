e882e9b73ca54f9791f35d32669d577707fe3db5
hieumai943 <hieunm.hrt@gmail.com>
2023-09-05 23:30:31 +0700

    làm lại aboutme slider

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './AboutMe.css'
 import slide1 from '../assets/Slide1.png';
 import slide2 from '../assets/Slide2.png';
 import slide3 from '../assets/Slide3.png';
@@ -7,48 +8,74 @@ import slide5 from '../assets/Slide5.png';
 import slide6 from '../assets/Slide6.png';
 import slide7 from '../assets/Slide7.png';
 import hrtLogo from '../assets/hrt_logo.png'
-import CustomCarousel from './CustomCarousel';
 
-const items = [
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+const data = [
   {
-    src: slide1,
-    altText: 'Slide 1',
-    caption: 'Slide 1'
+    avatar: slide1,
+    review: 'Cuộc thi Vietnamese Youth Technopreneur Challenge 2022 - VYTEC22'
   },
   {
-    src: slide2,
-    altText: 'Slide 2',
-    caption: 'Slide 2'
+    avatar: slide2,
+    review: 'Cuộc thi Vietnamese Youth Technopreneur Challenge 2021 - VYTEC21'
   },
   {
-    src: slide3,
-    altText: 'Slide 3',
-    caption: 'Slide 3'
+    avatar: slide3,
+    review: 'Cuộc thi Ý tưởng sáng tạo công nghệ'
   },
   {
-    src: slide4,
-    altText: 'Slide 4',
-    caption: 'Slide 4'
+    avatar: slide4,
+    name: 'Nana McBrown',
+    review: 'Cuộc thi Ươm mầm ý tưởng sáng tạo khởi nghiệp công nghệ - Innovative Entrepreneurs 2019'
   },
   {
-    src: slide6,
-    altText: 'Slide 6',
-    caption: 'Slide 6'
+    avatar: slide5,
+    review: 'Ngày hội truyến thông UET Connect'
   },
   {
-    src: slide7,
-    altText: 'Slide 7',
-    caption: 'Slide 7'
-  }
-];
+    avatar: slide6,
+    review: 'Cuộc thi UET Makerthon'
+  },
+  {
+    avatar: slide7,
+    review: 'Ngày hội việc làm UET Job Fair'
+  },
+  
+]
 export const AboutMe = () => {
   return (
-    <section id='aboutme'>
-      <div className='aboutMe'>
-        <h1 className='textAboutUs'>VỀ CHÚNG TÔI</h1>
-        <img className='logoCLB' src = {hrtLogo}></img>
-          <CustomCarousel items={items}/>
-      </div>
-      </section>
+    <section id='testimonials'>
+    <h1>VỀ CHÚNG TÔI</h1>
+    <img src={hrtLogo} id='hrtlogo'></img>
+    <h1>CÂU LẠC BỘ HRTECH</h1>
+
+    <Swiper className="container testimonials__container"
+    // install Swiper modules
+    modules={[Pagination]}
+    spaceBetween={40}
+    slidesPerView={1}
+    pagination={{ clickable: true }}
+    >
+      {
+        data.map(({avatar, name, review}, index) => {
+          return (
+            <SwiperSlide key={index} className='testimonial'>
+              <div className="client__avatar">
+                <img src={avatar}/>       
+              </div>
+             
+                <span className='client__review'>
+                  {review}
+                </span>
+            </SwiperSlide>
+          )
+        })
+      }  
+    </Swiper>
+  </section>
   )
 }
