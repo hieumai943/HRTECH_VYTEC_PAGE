a024d8d2386a67995fb33479527e82dd06b69cf1
hieumai943 <hieunm.hrt@gmail.com>
2023-10-05 21:38:39 +0700

    fix timeline

--- src/components/thele.js
+++ src/components/thele.js
@@ -8,24 +8,24 @@ export const Thele = () => {
         <div className="overlay"></div>
         <div className="content">
             <h1>THỂ LỆ</h1>
-            <p>ĐỐI TƯỢNG THAM GIA</p>
+            <h4>ĐỐI TƯỢNG THAM GIA</h4>
             <ul>
               <li>Sinh viên các trường Đại học/ Học viện trên cả nước.</li>
               <li>Các đội dự thi theo, hình thức nhóm, mỗi nhóm từ 3 đến 5 thành viên, các thành viên đều phải đang còn là sinh viên hoặc mới ra trường không quá một năm.</li>
             </ul>
           
-            <p>CÁC VÒNG THI</p>
+            <h4>CÁC VÒNG THI</h4>
             <ul>
               <li>Vòng 1: Online nộp dự án. </li>
               <li>Vòng 2: Bán kết offline.</li>
               <li>Vòng 3: Chung kết xét duyệt dự án, thuyết trình, tranh biện.</li> 
             </ul>
 
-            <p>CÁC BUỔI TRAINING</p>
+            <h4>CÁC BUỔI TRAINING</h4>
             <ul>
               <li>Chuẩn bị cho các đội thi 5 nội dung: Chọn lọc ý tưởng công nghệ, Marketing, Pitching, Quản trị sản phẩm, Kỹ năng hùng biện. </li>
             </ul>
-            <p>THỜI GIAN DỰ KIẾN</p>
+            <h4>THỜI GIAN DỰ KIẾN</h4>
             <ul>
               <li>Tiến độ thi diễn ra từ 09/10-09/12/2023. Đăng ký tham gia từ 09/10-23/10, nộp dự án trước 04/11.</li>
             </ul>
