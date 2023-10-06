b8435354d5bac61435ba50cdb6fb29aeea6a0eaa
dinh_namle <nam2004nam@gmail.com>
2023-10-06 17:31:33 +0700

    fix giai thuong

--- src/components/GiaiThuong.js
+++ src/components/GiaiThuong.js
@@ -15,27 +15,27 @@ export const GiaiThuong = () => {
             <h1>GIẢI THƯỞNG</h1>
             <div class="achieve">
             <img src={achieve1} />
-            <p><span>01 giải Nhất:</span> 10,000,000 VND tiền mặt + giấy chứng nhận + kỷ niệm chương.
+            <p><span>01 giải Nhất:</span> 5,000,000 VND tiền mặt + giấy chứng nhận + kỷ niệm chương.
             </p>
             </div>
             <div class="achieve">
             <img src={achieve2} />
-            <p> <span>01 giải Nhì:</span> 6,000,000 VND tiền mặt + giấy chứng nhận + kỷ niệm chương.
+            <p> <span>01 giải Nhì:</span> 3,000,000 VND tiền mặt + giấy chứng nhận + kỷ niệm chương.
             </p>
             </div>
             <div class="achieve">
             <img src={achieve3} />
-            <p><span>01 giải Ba:</span> 4,000,000 VND tiền mặt + giấy chứng nhận + kỷ niệm chương.
+            <p><span>01 giải Ba:</span> 2,000,000 VND tiền mặt + giấy chứng nhận + kỷ niệm chương.
             </p>
             </div>
             <div class="achieve">   
             <img src={achieve4} />
-            <p><span>01 giải Đội thi được yêu thích nhất:</span> 500,000 VND tiền mặt + giấy chứng nhận + kỷ niệm chương.
+            <p><span>01 giải Đội thi được yêu thích nhất:</span> 1,000,000 VND tiền mặt + giấy chứng nhận + kỷ niệm chương.
             </p>
             </div>
             <div class="achieve">
             <img src={achieve5} />
-            <p><span>01 giải Đội thi triển vọng nhất:</span> 01 giải Đội thi triển vọng nhất: 500,000 VND tiền mặt + giấy chứng nhận + kỷ niệm chương.
+            <p><span>01 giải Đội thi triển vọng nhất:</span> 01 giải Đội thi triển vọng nhất: 1,000,000 VND tiền mặt + giấy chứng nhận + kỷ niệm chương.
             </p>
             </div>
         </div>
